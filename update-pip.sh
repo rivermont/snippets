493dc5e2e9504af3776fb53fe0c115bcf12afb34
rivermont <william.11bennett@gmail.com>
2017-10-26 23:01:16 -0400

    Updated update-pip.sh

--- update-pip.sh
+++ update-pip.sh
@@ -1,1 +1,1 @@
-sudo pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip3 install -U
+sudo -H pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip3 install -U
