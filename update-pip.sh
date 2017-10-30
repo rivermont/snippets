6634b6c7ee31bf71c512c94f68f6dfb43b8411bf
rivermont <william.11bennett@gmail.com>
2017-10-30 19:07:47 -0400

    Update both Python3 and Python2 packages.

--- update-pip.sh
+++ update-pip.sh
@@ -1,1 +1,2 @@
 sudo -H pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip3 install -U
+sudo -H pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip2 install -U
